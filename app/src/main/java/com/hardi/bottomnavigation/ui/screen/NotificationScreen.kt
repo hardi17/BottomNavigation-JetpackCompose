6f2da902b424a9cc2fce67c35b05361518d9324a
Hardi Rachh <shahhardi71@gmail.com>
2024-03-23 22:33:24 +0000

    Added navigation depencies and screen for bottom navigation bar.

---
+++ app/src/main/java/com/hardi/bottomnavigation/ui/screen/NotificationScreen.kt
@@ -0,0 +1,28 @@
+package com.hardi.bottomnavigation.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun NotificationScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcom to, Notification Screen",
+            fontFamily = FontFamily.Cursive,
+            fontSize = 30.sp,
+            color = Color.Black
+        )
+    }
+}
